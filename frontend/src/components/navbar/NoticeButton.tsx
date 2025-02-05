7616b5b73874256d551155d5f39a2d720cedb603
Jaehoon-So <jhspacelover@naver.com>
2025-02-05 18:47:17 +0900

    fix: strokWidth -> strok-width

--- frontend/src/components/navbar/NoticeButton.tsx
+++ frontend/src/components/navbar/NoticeButton.tsx
@@ -6,7 +6,7 @@ export default function NoticeButton() {
     <>
       <Link className={styles.link} to="/notices">
         <button className={styles.navButton}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                stroke="currentColor"
                className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round"
