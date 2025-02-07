b1fe00c5ce6dbaea047513dc613a6f572a119a5c
Jaehoon-So <jhspacelover@naver.com>
2025-02-08 00:48:33 +0900

    fix: id로 퀴즈를 조회할 수 없는 버그 수정

--- backend/src/main/java/com/poolygo/quiz/domain/Quiz.java
+++ backend/src/main/java/com/poolygo/quiz/domain/Quiz.java
@@ -1,7 +1,6 @@
 package com.poolygo.quiz.domain;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
@@ -9,6 +8,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,7 +18,7 @@ import java.util.List;
 @Builder
 public class Quiz {
     @Id
-    @Field(name = "_id")
+    @Field(name = "_id", targetType = FieldType.OBJECT_ID)
     private String id;
 
     @CreatedDate
--- frontend/src/components/quiz/single/QuizPage.tsx
+++ frontend/src/components/quiz/single/QuizPage.tsx
@@ -1,12 +1,23 @@
-import {useEffect, useId} from "react";
-import {useParams} from "react-router-dom";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {axiosJwtInstance} from "../../../global/configuration/axios.ts";
+import {handleError} from "../../../global/error/error.ts";
+import axios from "axios";
 
 const QuizPage = () => {
 
   const {quizId} = useParams();
 
   useEffect(() => {
-
+    // console.log(`/api/quiz/${quizId}`);
+    axiosJwtInstance.get(`/api/quiz/${quizId}`)
+      .then((response) => {
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+        handleError(error);
+      })
   }, []);
 
   return (
